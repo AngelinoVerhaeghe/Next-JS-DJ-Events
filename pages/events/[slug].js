14d9f9f9e01b53cdb388f35866041b9a35552935
Sonicastra <Sonicastra@telenet.be>
2021-06-15 14:38:49 +0200

    add single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,96 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import Image from "next/image";
+import Link from "next/link";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 
-function EventPage() {
+function EventPage({ event }) {
+  const deleteEvent = (element) => {
+    console.log("delete");
+  };
   return (
     <Layout>
-      <h1>My Event</h1>
+      <div className="space-y-2">
+        <div className="flex items-cente space-x-3 justify-end">
+          <Link href={`/events/edit/${event.id}`}>
+            <a className="flex items-center text-blue-600">
+              <FaPencilAlt className="mr-2" /> Edit Event
+            </a>
+          </Link>
+          <a
+            href="#"
+            className="flex items-center text-red-600"
+            onClick={deleteEvent}
+          >
+            <FaTimes className="mr-2" /> Delete Event
+          </a>
+        </div>
+        <span className="text-sm text-gray-600">
+          {event.date} and {event.time}
+        </span>
+
+        <h1 className="text-2xl font-bold">{event.name}</h1>
+
+        {/* Look if the image exists */}
+        {event.image && (
+          <div>
+            <Image src={event.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3 className="font-bold tracking-wide">Performers:</h3>
+        <p className="text-sm text-gray-600">{event.performers}</p>
+        <h3 className="font-bold tracking-wide">Description:</h3>
+        <p className="text-md text-gray-600">{event.description}</p>
+        <h3 className="font-bold tracking-wide">Venue:</h3>
+        <p className="text-sm text-gray-600">{event.address}</p>
+        <div className="pt-7">
+          <Link href="/events">
+            <a className="py-2 px-4 bg-gray-600 rounded-lg text-white font-medium hover:bg-gray-800 transition duration-200 ease-in-out">
+              Back
+            </a>
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
 
 export default EventPage;
+
+/* Server side */
+/* export async function getServerSideProps({ query: { slug } }) {
+  const response = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await response.json();
+  return {
+    props: {
+      event: events[0],
+    },
+  };
+} */
+
+export async function getStaticPaths() {
+  /* Get All Data */
+  const response = await fetch(`${API_URL}/api/events`);
+  const events = await response.json();
+
+  const paths = events.map((event) => ({
+    params: { slug: event.slug },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+/* Get properties at build time */
+export async function getStaticProps({ params: { slug } }) {
+  const response = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await response.json();
+  return {
+    props: {
+      event: events[0],
+      revalidate: 1,
+    },
+  };
+}
