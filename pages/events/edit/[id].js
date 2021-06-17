458074f6b8b01f4da68e2258e898073e3ed2b85f
Sonicastra <Sonicastra@telenet.be>
2021-06-17 10:27:40 +0200

    Added Edit event and Image preview

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,25 +1,36 @@
+import moment from "moment";
+import { FaImage } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 /* Form with INPUT so add useState */
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Image from "next/image";
 import Layout from "@/components/Layout";
 /* Post request add API_URL */
 import { API_URL } from "@/config/index";
 
-export default function AddEventPage() {
+export default function EditEventPage({ event }) {
   const router = useRouter();
   /* Get all input values */
+  /* Set them with the event. functionality */
   const [values, setValues] = useState({
-    name: "",
-    performers: "",
-    venue: "",
-    address: "",
-    date: "",
-    time: "",
-    description: "",
+    name: event.name,
+    performers: event.performers,
+    venue: event.venue,
+    address: event.address,
+    /* Npm install moment for date access and import moment*/
+    date: event.date,
+    time: event.time,
+    description: event.description,
   });
+
+  /* IMAGE Preview useState()*/
+  /* Check if there is a image */
+  const [imagePreview, setImagePreview] = useState(
+    event.image ? event.image.formats.thumbnail.url : null
+  );
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -33,9 +44,9 @@ export default function AddEventPage() {
       toast.error("Please fill in all fields!");
     }
 
-    /* All fields are filled in, sending a POST request to Strapi backend */
-    const response = await fetch(`${API_URL}/events`, {
-      method: "POST",
+    /* All fields are filled in, sending a PUT request to Strapi backend and adding the event.id*/
+    const response = await fetch(`${API_URL}/events/${event.id}`, {
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
@@ -61,8 +72,8 @@ export default function AddEventPage() {
   };
 
   return (
-    <Layout title="Add New Event">
-      <h1 className="text-3xl font-bold pb-8">Add Event</h1>
+    <Layout title="Edit Event">
+      <h1 className="text-3xl font-bold pb-8">Edit Event</h1>
       <ToastContainer />
       <form onSubmit={handleSubmit}>
         <div className="space-y-4 px-2 md:px-0">
@@ -127,7 +138,8 @@ export default function AddEventPage() {
               className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-md sm:text-sm border-gray-300"
               id="date"
               name="date"
-              value={values.date}
+              /* Set value with moment package moment().format('yyyy-DD-MM') */
+              value={moment(values.date).format("yyyy-DD-MM")}
               onChange={handleInputChange}
             />
           </div>
@@ -145,7 +157,7 @@ export default function AddEventPage() {
             />
           </div>
         </div>
-        <div className="px-2 mt-4">
+        <div className="mt-4">
           <label htmlFor="description" className="font-medium">
             Event Description
           </label>
@@ -161,11 +173,31 @@ export default function AddEventPage() {
         <div className="mt-8">
           <input
             type="submit"
-            value="Add Event"
+            value="Update Event"
             className="py-2 px-4 w-full block cursor-pointer bg-indigo-500 rounded-lg text-white font-medium hover:bg-indigo-700 transition duration-200 ease-in-out"
           />
         </div>
       </form>
+
+      <h2 className="text-3xl font-bold pt-6 pb-4">Event Image</h2>
+      {/* Image preview here check if there is a image preview + import Image from next */}
+      {imagePreview ? (
+        <Image src={imagePreview} height={100} width={170} />
+      ) : (
+        <div>
+          <p>No image uploaded</p>
+        </div>
+      )}
+
+      <div className="pt-4">
+        {/* Import react-icons */}
+        <button
+          type="button"
+          className="py-2 px-4 flex items-center bg-indigo-600 rounded-lg text-white font-medium hover:bg-indigo-800 transition duration-200 ease-in-out"
+        >
+          <FaImage className="mr-2" /> Set Image
+        </button>
+      </div>
 
       <div className="mt-10">
         <Link href="/events">
@@ -176,4 +208,18 @@ export default function AddEventPage() {
       </div>
     </Layout>
   );
+}
+
+/* Get that event that u want to edit */
+export async function getServerSideProps({ params: { id } }) {
+  /* Get response -> event.id */
+  const response = await fetch(`${API_URL}/events/${id}`);
+  const event = await response.json();
+
+  /* Return prop and add it to page {event} */
+  return {
+    props: {
+      event,
+    },
+  };
 }
