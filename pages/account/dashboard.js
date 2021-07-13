18c9a249ec4b06ec1ab16e88bcd422ab60c53737
Sonicastra <Sonicastra@telenet.be>
2021-07-13 16:46:55 +0200

    added serverSideProps to the dashboard page, get access to user events only

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,10 +1,34 @@
 // Bring in the Layout
 import Layout from "@/components/Layout";
+import { parseCookies } from "@/helpers/index";
+import { API_URL } from "@/config/index";
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+  console.log(events);
   return (
     <Layout title="User | Dashboard">
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+//Get jwt token
+export async function getServerSideProps({ req }) {
+  // Get token out of request
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
